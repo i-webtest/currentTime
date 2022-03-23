905a86d60eacd076aed75d893ce8239dc2aa67ca
i-webtest <stasbass80@gmail.com>
2022-03-23 10:49:17 +0300

    правки

--- script.js
+++ script.js
@@ -6,18 +6,20 @@ const time = document.querySelector(".time");
 const happyNewYear = document.querySelector(".remained");
 
 const timer = () => {
-  let currentTime = new Date();
+  const currentTime = new Date();
 
-  let dateStop = new Date("1 january 2023").getTime();
-  let timeRemaining = (dateStop - currentTime) / 1000;
-  let newYear = Math.floor(timeRemaining / 60 / 60 / 24);
+  const date = currentTime.getTime();
+  const dateStop = new Date("1 january 2024").getTime();
+  const timeRemaining = (dateStop - date) / 1000;
+  const newYear = Math.floor(timeRemaining / 60 / 60 / 24);
 
   let hour = currentTime.getHours();
   let minutes = currentTime.getMinutes();
   let seconds = currentTime.getSeconds();
 
-  let dayOfWeek = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
-  let days = currentTime.getDay();
+  const dayOfWeek = new Date().toLocaleDateString("ru-RU", { weekday: "long" });
+
+  // const days = currentTime.getDay();
 
   minutes = minutes < 10 ? "0" + minutes : minutes;
   seconds = seconds < 10 ? "0" + seconds : seconds;
@@ -30,11 +32,14 @@ const timer = () => {
     timeDay.textContent = "Доброе утро!";
   } else if (hour <= 18) {
     timeDay.textContent = "Добрый день!";
+  } else if (hour <= 23) {
+    timeDay.textContent = "Добрый вечер!";
   }
 
-  weekDay.innerHTML = dayOfWeek[days];
+  weekDay.innerHTML = dayOfWeek[0].toUpperCase() + dayOfWeek.slice(1);
   happyNewYear.innerHTML = newYear;
 
   setTimeout(timer, 1000);
 };
+
 timer();
